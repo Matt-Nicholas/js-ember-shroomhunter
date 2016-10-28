6909b1a16bc28d8e69e9d714f895995042e5c6f0
Matt Nicholas <fostermatt82@gmial.com>
2016-10-28 13:17:35 -0700

    add update function and dynamic routing

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   updateQuestionForm: false,
-  actions:{
-    delete(question) {
-      update(question, params) {
-        this.sendAction('update', question, params);
-      },
-      if (confirm('Are you sure you want to delete this question?')) {
-        this.sendAction('destroyQuestion', question);
-      }
-    }
-  }
+  // actions:{
+    // delete(question) {
+    //   update(question, params) {
+    //     this.sendAction('update', question, params);
+    //   },
+    //   if (confirm('Are you sure you want to delete this question?')) {
+    //     this.sendAction('destroyQuestion', question);
+    //   }
+    // }
+  // }
 });
