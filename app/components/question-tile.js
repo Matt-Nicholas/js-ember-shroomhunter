7d727ca28614b050df9497d2714335a7148bdba4
Matt Nicholas <fostermatt82@gmial.com>
2016-10-28 12:49:53 -0700

    add update function

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,8 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-actions:{
-  delete(question) {
+  updateQuestionForm: false,
+  actions:{
+    delete(question) {
+      update(question, params) {
+        this.sendAction('update', question, params);
+      },
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
       }
