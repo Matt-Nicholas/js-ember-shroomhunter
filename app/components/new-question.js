ca9542d183d01d741fc1c7016c3502ff1e06344a
Matt Nicholas <fostermatt82@gmial.com>
2016-10-28 14:29:56 -0700

    add answers form with one to many relationship to questions

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-    actions: {
-      saveQuestion() {
-        var params = {
-          content: this.get('content'),
-          author: this.get('author'),
-          date: this.get('date'),
-        };
-        this.sendAction('saveQuestion', params);
-      }
+  actions: {
+    saveQuestion() {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        date: this.get('date'),
+      };
+      this.sendAction('saveQuestion', params);
     }
+  }
 });
